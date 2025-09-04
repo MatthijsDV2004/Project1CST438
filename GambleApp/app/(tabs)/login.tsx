d055714960b1dcfd6cf99e745cab30ca193073b7
Michael Garcia <157657140+Generic-Mann@users.noreply.github.com>
2025-09-04 13:03:20 -0700

    Started login page

---
+++ GambleApp/app/(tabs)/login.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import {
+  Alert,
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import { Image } from 'expo-image';
+import { Feather } from '@expo/vector-icons';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
