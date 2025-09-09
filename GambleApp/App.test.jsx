49e1414ac6e5e151eb4d4a00a694dd8bdb2cbeae
MatthijsDV2004 <mdevries@csumb.edu>
2025-09-09 13:27:45 -0700

    Update App.test.jsx

--- GambleApp/App.test.jsx
+++ GambleApp/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import RootLayout from '../GambleApp/app/_layouts';
+import RootLayout from '../GambleApp/app/_layout';
 
 it('renders the app root without crashing', () => {
   const tree = render(<RootLayout />);
