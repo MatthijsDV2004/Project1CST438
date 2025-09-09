5d9a6aa766d8e41dc92298802d552fc55999597c
MatthijsDV2004 <mdevries@csumb.edu>
2025-09-09 13:22:02 -0700

    Update App.test.jsx

--- GambleApp/App.test.jsx
+++ GambleApp/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import App from '../App';
+import App from '../GambleApp/App';
 
 it('renders the app root without crashing', () => {
   const tree = render(<App />);
